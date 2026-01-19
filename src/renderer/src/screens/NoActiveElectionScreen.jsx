8699de64830cba982d10c635711883ef66f8c7fe
Jiyad <mdjiyade@gmail.com>
2026-01-19 16:37:42 +0530

    Added NoActiveElectionScreen

---
+++ src/renderer/src/screens/NoActiveElectionScreen.jsx
@@ -0,0 +1,23 @@
+import Button from '../components/Button'
+
+const NoActiveElectionScreen = () => {
+  return (
+    <div className="flex items-center flex-1 md:px-3 lg:px-7 text-sm gap-6 sm:py-3">
+      <div className="flex flex-col items-center gap-8 px-3 py-10 w-full">
+        <div className="flex justify-center">
+          <h2 className="text-center text-primary-light dark:text-primary-dark text-2xl md:text-3xl lg:text-4xl font-black">
+            No Elections Are Currently Active Or Scheduled
+          </h2>
+        </div>
+        <Button
+          className="flex flex-col justify-center items-center py-3 px-6 gap-1 bg-accent hover:bg-button-hover"
+          onClick={() => window.electron.quitApp()}
+        >
+          Exit the App
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default NoActiveElectionScreen
