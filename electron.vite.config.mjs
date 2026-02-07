f075839a5411f290d0aa9ffba342b0beb27d02a5
Jiyad <mdjiyade@gmail.com>
2026-02-07 10:32:46 +0530

    Added build configurations

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,17 +1,25 @@
 import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
   renderer: {
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    assetsInclude: ['**/*.mp3'],
+    build: {
+      assetsInlineLimit: 0
+    }
   }
 })
