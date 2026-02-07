6473b99153aff6456d0a3fea37d259c0242c0db3
Jiyad <mdjiyade@gmail.com>
2026-02-07 10:33:17 +0530

    Added system activated but voting finished state

--- src/renderer/src/screens/LockedScreen.jsx
+++ src/renderer/src/screens/LockedScreen.jsx
@@ -1,6 +1,6 @@
 import Button from '../components/Button'
 
-const LockedScreen = ({ status }) => {
+const LockedScreen = ({ status, systemActivated }) => {
   return (
     <div className="flex items-center flex-1 md:px-3 lg:px-7 text-sm gap-6 sm:py-3">
       <div className="flex flex-col items-center gap-8 px-3 py-10 w-full">
@@ -15,16 +15,26 @@ const LockedScreen = ({ status }) => {
               </p>
             </>
           )}
-          {['post-voting', 'closed'].includes(status) && (
-            <>
-              <h2 className="text-center text-primary-light dark:text-primary-dark text-2xl md:text-3xl lg:text-4xl font-black">
-                Activation Unavailable
-              </h2>
-              <p className="text-secondary-light dark:text-secondary-dark text-base">
-                The voting phase has concluded. System activation is no longer available
-              </p>
-            </>
-          )}
+          {['post-voting', 'closed'].includes(status) &&
+            (systemActivated ? (
+              <>
+                <h2 className="text-center text-primary-light dark:text-primary-dark text-2xl md:text-3xl lg:text-4xl font-black">
+                  Voting Concluded
+                </h2>
+                <p className="text-secondary-light dark:text-secondary-dark text-base">
+                  The voting process has ended. No further actions are available at this stage
+                </p>
+              </>
+            ) : (
+              <>
+                <h2 className="text-center text-primary-light dark:text-primary-dark text-2xl md:text-3xl lg:text-4xl font-black">
+                  Activation Unavailable
+                </h2>
+                <p className="text-secondary-light dark:text-secondary-dark text-base">
+                  The voting phase has concluded. System activation is no longer available
+                </p>
+              </>
+            ))}
         </div>
         <Button
           className="flex flex-col justify-center items-center py-3 px-6 gap-1 bg-accent hover:bg-button-hover"
